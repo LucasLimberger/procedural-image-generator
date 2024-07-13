4ce37b34c273f2e6517a11e78e64eca66e11c16a
LucasLimberger <86330138+LucasLimberger@users.noreply.github.com>
2024-07-13 02:27:37 -0300

    Modo lento com contagem de passos

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
 import styles from "./Content.module.css";
+import { useCallback, useEffect, useState } from "react";
+import Controls from "./Controls";
 import TilesetSelector from "./TilesetSelector";
 import Grid from "./Grid";
 import PauseOverlay from "./PauseOverlay";
 import WaveFunctionCollapseGrid from "../scripts/WaveFunctionCollapseGrid";
 import { TILESETS, TilesetName, TileName } from "../data/tileData";
-import Controls from "./Controls";
 
 export default function Content() {
+  const intervalDuration = 100;
   const [running, setRunning] = useState(false);
+  const [stepsTaken, setStepsTaken] = useState(0);
   const [settings, setSettings] = useState({
     activeTileset: "terrain" as TilesetName,
     gridWidth: 10,
@@ -25,36 +27,41 @@ export default function Content() {
     )
   );
 
-  if (running && !wfcg.isDone && settings.playInstantly) {
-    wfcg.runUntilDone();
+  if (running && settings.playInstantly) {
+    while (!wfcg.isDone) {
+      wfcg.step();
+      setStepsTaken(prev => prev + 1);
+    }
   }
-  // useEffect(() => {
-  //   if (!running || wfcg.isDone) return;
-  //   if (settings.playInstantly) {
-  //     wfcg.runUntilDone();
-  //     setWfcg(wfcg);
-  //     return;
-  //   } else {
-  //     const interval = setInterval(() => {
-  //       wfcg.step();
-  //       setWfcg(wfcg);
-  //     }, 1000);
-  //     return () => clearInterval(interval);
-  //   }
-  // }, [running, wfcg]);
+
+  useEffect(() => {
+    if (!running || wfcg.isDone || settings.playInstantly) return;
+    const interval = setTimeout(() => {
+      wfcg.step();
+      setStepsTaken(prev => prev + 1);
+    }, intervalDuration);
+    return () => clearInterval(interval);
+  }, [running, wfcg, stepsTaken, settings.playInstantly]);
+
+  useEffect(() => {
+    setStepsTaken(0);
+  }, [wfcg, settings.gridWidth, settings.gridHeight, settings.activeTileset]);
+
   function rerun() {
     wfcg.clear();
+    setStepsTaken(0);
   }
+
   const handleTilesetChange = useCallback((newTileset: TilesetName) => {
     setSettings(prev => ({ ...prev, activeTileset: newTileset }));
-      setWfcg(
+    setWfcg(
       prev =>
         new WaveFunctionCollapseGrid<TileName>(
           prev.width,
           prev.height,
           TILESETS[newTileset]
         )
-      );
+    );
   }, []);
   function handleWidthChange(newWidth: number) {
     setSettings(prev => ({ ...prev, gridWidth: newWidth }));
@@ -67,34 +74,33 @@ export default function Content() {
   function handlePlayModeChange(playInstantly: boolean) {
     setSettings(prev => ({ ...prev, playInstantly: playInstantly }));
   }
+
   return (
-    <>
-      <main className={styles.main}>
-        <Controls
-          gridWidth={settings.gridWidth}
-          gridHeight={settings.gridHeight}
-          playInstantly={settings.playInstantly}
-          onWidthChange={handleWidthChange}
-          onHeightChange={handleHeightChange}
-          onPlayModeChange={handlePlayModeChange}
-          onRerunRequest={rerun}
+    <main className={styles.main}>
+      <Controls
+        gridWidth={settings.gridWidth}
+        gridHeight={settings.gridHeight}
+        playInstantly={settings.playInstantly}
+        onWidthChange={handleWidthChange}
+        onHeightChange={handleHeightChange}
+        onPlayModeChange={handlePlayModeChange}
+        onRerunRequest={rerun}
+      />
+      <section className={styles.bottomSection}>
+        <TilesetSelector
+          value={settings.activeTileset}
+          onSelect={handleTilesetChange}
+        />
+        <div className={styles.display}>
+          <Grid
+            width={settings.gridWidth}
+            height={settings.gridHeight}
+            cells={[...wfcg.iterCells()]}
+            tilesetName={settings.activeTileset}
           />
-        <section className={styles.bottomSection}>
-          <TilesetSelector
-            value={settings.activeTileset}
-            onSelect={handleTilesetChange}
-          />
-          <div className={styles.display}>
-            <Grid
-              width={settings.gridWidth}
-              height={settings.gridHeight}
-              cells={[...wfcg.iterCells()]}
-              tilesetName={settings.activeTileset}
-            />
-            {!running && <PauseOverlay onDismiss={() => setRunning(true)} />}
-          </div>
-        </section>
-      </main>
-    </>
+          {!running && <PauseOverlay onDismiss={() => setRunning(true)} />}
+        </div>
+      </section>
+    </main>
   );
 }
