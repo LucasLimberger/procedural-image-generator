d4c3e5cb0a22a262db939e201e1a3fb193688801
LucasLimberger <86330138+LucasLimberger@users.noreply.github.com>
2025-02-26 23:59:14 -0300

    Redirecionamento automático de locales inválidos

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,26 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { DEFAULT_LOCALE, SUPPORTED_LOCALES } from "./data/languageData";
-
-export function middleware(request: NextRequest) {
-  const languageHeader = request.headers.get("accept-language") ?? "*";
-  const proposals = parseLanguageHeader(languageHeader);
-  const locale = matchLocale(proposals, SUPPORTED_LOCALES, DEFAULT_LOCALE);
-  request.nextUrl.pathname = `/${locale}${request.nextUrl.pathname}`;
-  return NextResponse.redirect(request.nextUrl);
-}
+import {
+  DEFAULT_LOCALE,
+  SUPPORTED_LOCALES,
+  type SupportedLocale,
+} from "./data/languageData";
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/:locale?"],
 };
+
+export function middleware(request: NextRequest) {
+  const locale = request.nextUrl.pathname.split("/")[1];
+  if (SUPPORTED_LOCALES.includes(locale as SupportedLocale)) {
+    return;
+  }
+
+  const languageHeader = request.headers.get("accept-language") ?? "*";
+  const proposals = parseLanguageHeader(languageHeader);
+  const newLocale = matchLocale(proposals, SUPPORTED_LOCALES, DEFAULT_LOCALE);
+  request.nextUrl.pathname = "/" + newLocale;
+  return NextResponse.redirect(request.nextUrl);
+}
 
 const LOCALE_REG_EXP = /(\*|[\w\-]+)(?:;q=([\d.]+))?/i;
 function parseLanguageHeader(languageHeader: string) {
